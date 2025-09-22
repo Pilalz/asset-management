381478bda4f4e735d70cc2d77173fffe97c2de8a
Rafi Zahir <rafi.zahir@borneo.co.id>
2025-09-22 17:33:12 +0700

    dark mode logo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -31,8 +31,13 @@
                             <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                         </svg>
                     </button>
-                    <a href="{{ route('dashboard') }}" class="flex ms-2 md:me-24">
+                    
+                    <a href="{{ route('dashboard') }}" class="flex ms-2 md:me-24 dark:hidden">
                         <img src="{{ asset('images/logo.svg') }}" class="h-8 me-3" alt="Asset Management Logo" />
+                        <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Asset Management</span>
+                    </a>
+                    <a href="{{ route('dashboard') }}" class="hidden ms-2 md:me-24 dark:flex">
+                        <img src="{{ asset('images/logo-dark.svg') }}" class="h-8 me-3" alt="Asset Management Logo" />
                         <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Asset Management</span>
                     </a>
                 </div>
